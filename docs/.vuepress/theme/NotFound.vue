7fc44558704de7415823644ae99b8f1b419ebe55
Eko Eryanto <ekoeryanto@gmail.com>
2018-08-25 01:08:22 +0700

    chore: update 404 page

--- docs/.vuepress/theme/NotFound.vue
+++ docs/.vuepress/theme/NotFound.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="theme-container">
-    <div class="content">
-      <h1>404</h1>
-      <blockquote>{{ getMsg() }}</blockquote>
-      <router-link to="/">Take me home.</router-link>
-    </div>
-  </div>
+  <v-app dark>
+    <v-container fill-height>
+      <v-layout align-center>
+        <v-flex class="text-xs-center">
+          <h1>404</h1>
+          <blockquote>{{ getMsg() }}</blockquote>
+          <v-btn flat color="error" to="/">Take me home.</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
+import { VBtn } from 'vuetify';
+
 const msgs = [
   `There's nothing here.`,
   `How did we get here?`,
@@ -17,6 +23,9 @@ const msgs = [
 ];
 
 export default {
+  components: {
+    VBtn,
+  },
   methods: {
     getMsg() {
       return msgs[Math.floor(Math.random() * msgs.length)];
