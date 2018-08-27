c66cfec611b567aa4d176974cc35767682236978
Eko Eryanto <ekoeryanto@gmail.com>
2018-08-27 23:08:58 +0700

    chore: disable ga

--- docs/.vuepress/components/ContactPage.vue
+++ docs/.vuepress/components/ContactPage.vue
@@ -1,5 +1,136 @@
 <template>
   <div>
+    <v-container grid-list-xl>
+      <v-layout
+        row
+        wrap
+        grid-list-md
+      >
+
+        <v-flex
+          sm12
+          md6
+        >
+          <v-form
+            ref="contact"
+            v-model="form.valid"
+            name="contact"
+            column
+            method="post"
+            @submit.prevent="send"
+          >
+            <div style="display: none">
+              <input name="bot-field">
+            </div>
+            <v-text-field
+              v-model="form.name"
+              label="Name"
+              name="name"
+              :rules="form.nameRules"
+              autocomplete="off"
+              clearable
+              flex
+              required
+            />
+
+            <v-text-field
+              v-model="form.email"
+              label="E-mail"
+              autocomplete="off"
+              name="email"
+              :rules="form.emailRules"
+              clearable
+              required
+            />
+
+            <v-textarea
+              v-model="form.message"
+              label="Message"
+              :rules="form.messageRules"
+              name="message"
+              rows="3"
+              counter="160"
+              clearable
+              no-resize
+              required
+            />
+
+            <v-layout
+              row
+              grid-list-md
+              align-center
+              justify-space-between
+              class="mt-3"
+            >
+              <v-btn
+                color="secondary"
+                type="submit"
+                :disabled="!form.valid"
+              >
+                Send
+              </v-btn>
+            </v-layout>
+          </v-form>
+        </v-flex>
+
+        <v-flex>
+          <v-list style="background: transparent">
+            <v-list-tile v-if="email">
+              <v-list-tile-action>
+                <v-icon>email</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title v-text="email.join(', ')" />
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile v-if="phone">
+              <v-list-tile-action>
+                <v-icon>phone</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title v-text="phone.join(', ')" />
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile v-if="fax">
+              <v-list-tile-action>
+                <v-icon>print</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title v-text="fax.join(', ')" />
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile v-if="address">
+              <v-list-tile-action>
+                <v-icon>place</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title v-text="address" />
+                <v-list-tile-sub-title
+                  v-if="address2"
+                  v-text="address2"
+                />
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+
+          <!-- <no-ssr class="mt-4">
+            <social-networks :networks="social" btn-class="mx-3" icon-size="2x"/>
+          </no-ssr> -->
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-snackbar
+      v-model="snackbar"
+      bottom
+      right
+    >
+      {{ snackMsg }}
+      <v-btn
+        flat
+        color="success"
+        @click.native="clear"
+      >OK</v-btn>
+    </v-snackbar>
   </div>
 </template>
 
