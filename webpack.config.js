06c46d4f53042b31599bd67b5e0577caeacffcc9
Eko Eryanto <ekoeryanto@gmail.com>
2018-08-29 10:15:52 +0700

    chore: cms use dynamic import

--- webpack.config.js
+++ webpack.config.js
@@ -82,6 +82,11 @@ module.exports = (e, a) => ({
     new HtmlWebpackExternalsPlugin({
       externals: [
         {
+          module: "netlify-identity-widget",
+          entry: "https://identity.netlify.com/v1/netlify-identity-widget.js",
+          global: "netlifyIdentity"
+        },
+        {
           module: "react",
           entry: "umd/react.production.min.js",
           global: "React"
