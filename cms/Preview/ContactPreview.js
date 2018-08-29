435bd758057d37bb898b20df6ac168ef202d0824
Eko Eryanto <ekoeryanto@gmail.com>
2018-08-30 00:32:07 +0700

    fix(cms): contact preview array error

--- cms/Preview/ContactPreview.js
+++ cms/Preview/ContactPreview.js
@@ -1,22 +1,19 @@
 import React from 'react'
 
-import SiteTitle from "@components/SiteTitle.vue";
-import ContactPage from "@components/ContactPage.vue";
-import { VContainer } from "vuetify/es5/components/VGrid"
+import SiteTitle from '@components/SiteTitle.vue'
+import ContactPage from '@components/ContactPage.vue'
+import { VContainer } from 'vuetify/es5/components/VGrid'
 export default ({ entry, getAsset, widgetFor, widgetsFor }) => {
   function _f(f) {
     return entry.getIn(['data', f])
   }
   return (
     <div>
-      <SiteTitle
-        title={_f('title')}
-        subtitle={_f('subtitle')}
-      />
+      <SiteTitle title={_f('title')} subtitle={_f('subtitle')} />
       <ContactPage
-        email={_f('email').toArray()}
-        phone={_f('phone').toArray()}
-        fax={_f('fax').toArray()}
+        email={_f('email') && _f('email').toArray()}
+        phone={_f('phone') && _f('phone').toArray()}
+        fax={_f('fax') && _f('fax').toArray()}
         address={_f('address')}
         address2={_f('address2')}
       />
