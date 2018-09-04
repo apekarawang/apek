e9d1457033608c441507403949eb61be408edbf6
Eko Eryanto <ekoeryanto@gmail.com>
2018-09-05 01:26:04 +0700

    feat(cms): youtube editor component

--- cms/cms.js
+++ cms/cms.js
@@ -7,8 +7,29 @@ Vue.component('ClientOnly', ClientOnly)
 import('netlify-cms').then(async ({ default: CMS }) => {
   const NetlifyCMSWidgetMaterialIcons = await import('netlify-cms-widget-material-icons')
   const NetlifyCMSWidgetColor = await import('netlify-cms-widget-native-color')
+
   CMS.registerWidget('material-icons', NetlifyCMSWidgetMaterialIcons.Control)
   CMS.registerWidget('color', NetlifyCMSWidgetColor.Control)
+  CMS.registerEditorComponent({
+    // Internal id of the component
+    id: 'youtube',
+    // Visible label
+    label: 'Youtube',
+    // Fields the user need to fill out when adding an instance of the component
+    fields: [{ name: 'id', label: 'Youtube Video ID', widget: 'string' }],
+    // Pattern to identify a block as being an instance of this component
+    pattern: /^youtube (\S+)$/,
+    // Function to extract data elements from the regexp match
+    fromBlock: match => match && { id: match[1] },
+    // Function to create a text block from an instance of this component
+    toBlock: o => `youtube ${o.id}`,
+    // Preview output for this component. Can either be a string or a React component
+    // (component gives better render performance)
+    toPreview: o =>
+      `<img src="http://img.youtube.com/vi/${
+        o.id
+      }/maxresdefault.jpg" alt="Youtube Video" />`,
+  })
 
   const HomePreview = await import('./Preview/HomePreview')
   const AboutPreview = await import('./Preview/AboutPreview')
